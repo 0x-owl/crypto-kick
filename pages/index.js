ea5a2cf9a42a0322e88b6c9bbb8589ee19412eb3
0xtomowl <tomowl@pm.me>
2022-04-04 22:28:06 -0300

    feat: Add semantic UI + use initial props method from next and allow server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import { React, Component } from 'react';
 import factory from '../ethereum/factory';
 
+
 class CampaignIndex extends Component {
 
-    async componentDidMount() {
+    // method provided by Next, allows to retrieve the
+    // initial props without instanciating the class which
+    // will call the render and it consumes a lot of of resources
+    static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
-        console.log(campaigns);
+        // provides the campaigns on instantiation as props to the class
+        // using componentDidMount requires a browser and tu make the request upon load
+        // this approach takes the server side rendering and preloads this before the
+        // component.
+        return { campaigns }
     }
 
     render () {
-        return <div>Campaings Index</div>
+        return <div> {this.props.campaigns[0]} Campaings Index</div>
     }
 }
 
