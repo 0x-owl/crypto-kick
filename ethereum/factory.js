e93c97782283910fe2c9f21828d03bef138936cc
0xtomowl <tomowl@pm.me>
2022-04-07 21:57:04 -0300

    update factory address on contract instance

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 // we seed a few using remix associating also our metamask for testing
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xECC705Dcc21dFC0e31b9ADdFDf7423376Ce94Aa6'
+    '0x1942e4214dc7d680827DF9Ca09140d037E0e6a99'
 );
 
 
